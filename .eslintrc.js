b5a12730f424667092225c49f8decb69e3b7893e
gusflopes <gusflopes86@gmail.com>
2019-10-26 22:19:57 -0400

    finished basic functionality

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,8 @@ module.exports = {
         extensions: [ '.jsx', '.js']
       }
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
   },
 };
